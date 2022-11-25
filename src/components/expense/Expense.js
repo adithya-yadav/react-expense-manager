80d2c0de288593ec61e204aaf0597ff8f02245ca
adithya-yadav <badriadithya7396@gmail.com>
2022-11-25 13:50:36 +0530

    single Expense message

--- src/components/expense/Expense.js
+++ src/components/expense/Expense.js
@@ -8,42 +8,57 @@ const Expense = (props) => {
   const [filteredYear, setFilteredYear] = useState("allExp");
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-    
   };
   let filteredExpense;
-  if(filteredYear=="allExp"){
-    filteredExpense = props.items
-  }else{
-    filteredExpense = props.items.filter((expense)=>{
-      return expense.date.getFullYear().toString() === filteredYear
-    })
+  if (filteredYear == "allExp") {
+    filteredExpense = props.items;
+  } else {
+    filteredExpense = props.items.filter((expense) => {
+      return expense.date.getFullYear().toString() === filteredYear;
+    });
   }
 
-  let ListContent=<p className="emptyList">No expense in this year</p>
+  let ListContent = <p className="emptyList">No expense in this year</p>;
 
-  if(filteredExpense.length > 0){
-    ListContent = filteredExpense.map((expense) =>{  
-      return  <ExpenseItem
+  if (filteredExpense.length > 1) {
+    ListContent = filteredExpense.map((expense) => {
+      return (
+        <ExpenseItem
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
           location={expense.location}
           key={expense.id}
+        />
+      );
+    });
+  } else if (filteredExpense.length === 1) {
+    ListContent = filteredExpense.map((expense) => {
+      return (
+        <div key={expense.id}>
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+            location={expense.location}
+            key={expense.id}
           />
-   } )
+          <p className="emptyList">Only single Expense here. Please add more...</p>
+        </div>
+      );
+    });
   }
 
   return (
-      <Card className="expense">
-        <div className="expenses">
-          <ExpenseFilter
-            selected={filteredYear}
-            onChangeFilter={filterChangeHandler}
-          />
-          {ListContent}
-        </div>
-      </Card>
-
+    <Card className="expense">
+      <div className="expenses">
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {ListContent}
+      </div>
+    </Card>
   );
 };
 
